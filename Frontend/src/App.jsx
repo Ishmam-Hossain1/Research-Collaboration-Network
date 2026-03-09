6475c11f7d2d9b815100ba5e9942659582c45961
Ishmam-Hossain1 <ishmam2898@gmail.com>
2026-03-09 16:40:27 +0600

    Push code to ishmam/collaboration-feature branch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,33 @@
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import Home from "./pages/Homepage";
+// import UserProfile from "./pages/UserProfile";
+// import Researchers from "./pages/Researchers";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Navigate to="/login" />} />
+//       <Route path="/login" element={<Login />} />
+//       <Route path="/signup" element={<Signup />} />
+//       <Route path="/home" element={<Home />} />
+//       <Route path="/profile" element={<UserProfile />} />
+//       <Route path="/researchers" element={<Researchers />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Homepage";
 import UserProfile from "./pages/UserProfile";
 import Researchers from "./pages/Researchers";
+import ResearcherProfile from "./pages/ResearcherProfile";
+import CollaborationRequests from "./pages/CollaborationRequests";
 
 function App() {
   return (
@@ -14,6 +38,11 @@ function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/profile" element={<UserProfile />} />
       <Route path="/researchers" element={<Researchers />} />
+      <Route path="/researchers/:id" element={<ResearcherProfile />} />
+      <Route
+        path="/collaboration-requests"
+        element={<CollaborationRequests />}
+      />
     </Routes>
   );
 }
