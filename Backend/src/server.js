479839f954b2775a8bda739558f774b1c28d83d2
Ishmam-Hossain1 <ishmam2898@gmail.com>
2026-04-21 04:49:08 +0600

    merge issue solved

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -210,10 +210,8 @@ app.use("/api/funding", fundingRoutes);
 app.use("/api/feedback", feedbackRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/messages", messageRoutes);
-<<<<<<< HEAD
-=======
 app.use("/api/milestones", milestoneRoutes);
->>>>>>> origin/ishmam/homepage-ui
+
 
 // Default route
 app.get("/", (req, res) => {
